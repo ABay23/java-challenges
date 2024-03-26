fc5a7cff36d0fb9a9a9f1337830724cbb44f1abc
ABay23 <alejandrobay23@gmail.com>
2024-03-25 22:03:43 -0400

    Product For Sale

---
+++ Challenges/Abstract Classes/src/dev/lpa/ProductForSale.java
@@ -0,0 +1,18 @@
+package dev.lpa;
+
+public abstract class ProductForSale {
+
+    protected String type;
+    protected double price;
+    protected String description;
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public double getSalesPrice(int qty){
+        return
+    }
+}
--- Challenges/Abstract Classes/src/dev/lpa/Store.java
+++ Challenges/Abstract Classes/src/dev/lpa/Store.java
@@ -1,3 +1,5 @@
+package dev.lpa;
+
 public class Store {
     public static void main(String[] args){
 
